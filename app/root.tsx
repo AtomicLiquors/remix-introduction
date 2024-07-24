5062cfe7778985c447243a89fe6a7ae3d058dfa7
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2024-07-24 16:01:33 +0900

    :sparkles: [tutorial] completed Create, Update on Contact

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,41 @@
+import { json } from "@remix-run/node";
 import {
   Form,
+  Link,
   Links,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import appStylesHref from "./app.css?url";
+import { createEmptyContact, getContacts } from "./data";
 
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
+};
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
+
+/*
+ * /app/root.tsx
+ * This is what we call the "Root Route".
+ * It's the first component in the UI that renders,
+ * so it typically contains the global layout for the page.
+ */
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -17,7 +46,6 @@ export default function App() {
       </head>
       <body>
         <div id="sidebar">
-          <h1>Remix Contacts</h1>
           <div>
             <Form id="search-form" role="search">
               <input
@@ -33,16 +61,46 @@ export default function App() {
               <button type="submit">New</button>
             </Form>
           </div>
+        <nav>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          </nav>
+          <h1>Remix Contacts</h1>
           <nav>
             <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <Link to={`/contacts/1`}>Your Name</Link>
               </li>
               <li>
                 <a href={`/contacts/2`}>Your Friend</a>
               </li>
             </ul>
           </nav>
+        </div>
+        <div id="detail">
+          <Outlet />
         </div>
 
         <ScrollRestoration />
