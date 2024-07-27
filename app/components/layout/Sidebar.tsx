8a94af7f617f587f194861c185611beb8d8e103b
chb <gyqls234@gmail.com>
2024-07-28 00:08:29 +0900

    :label: created skill component

---
+++ app/components/layout/Sidebar.tsx
@@ -0,0 +1,82 @@
+
+import {
+    Form,
+    Link,
+    NavLink,
+    useResolvedPath,
+} from "@remix-run/react";
+
+export default function Sidebar() {
+return <>
+{ location.pathname !== "/" && 
+        <div id="sidebar">
+          <div>
+            <Form id="search-form" role="search"
+              onChange={(event) => {
+                const isFirstSearch = q === null;
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}
+            >
+              <input
+                id="q"
+                aria-label="Search contacts"
+                className={searching ? "loading" : ""}
+                defaultValue={q || ""}
+                placeholder="Search"
+                type="search"
+                name="q"
+              />
+              <div id="search-spinner" aria-hidden hidden={!searching} />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      <Link to={`contacts/${contact.id}`}>
+                        {contact.first || contact.last ? (
+                          <>
+                            {contact.first} {contact.last}
+                          </>
+                        ) : (
+                          <i>No Name</i>
+                        )}{" "}
+                        {contact.favorite ? <span>★</span> : null}
+                      </Link>
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          </nav>
+          <h1>Remix Contacts</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to={`/contacts/1`}>Your Name</Link>
+              </li>
+              <li>
+                <a href={`/contacts/2`}>Your Friend</a>
+              </li>
+            </ul>
+          </nav>
+        </div>}
+        
+</>
+}
---
+++ app/components/skills/Skill.tsx
@@ -0,0 +1,7 @@
+
+export default function Skill() {
+    //return <div>2koooong2</div>
+    return <>
+    <div></div>
+    </>;
+}
