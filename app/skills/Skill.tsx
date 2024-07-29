abae44eac6b2a996e28b9c0e0a0b994a30a231d1
chb <gyqls234@gmail.com>
2024-07-30 06:25:30 +0900

    :lipstick: replaced grid with responsive flex

--- app/skills/Skill.tsx
+++ app/skills/Skill.tsx
@@ -6,7 +6,7 @@ const path = "/skills"
 const Skill: React.FC<SkillProps> = ({ title, content, img }) => {
   return (
     <>
-      <Center flex className="bg-white p-4 shadow-md rounded-lg">
+      <Center flex className="sm:w-1/2 md:w-1/3 lg:w-1/4 bg-white p-4 shadow-md rounded-lg">
         <img src={`${path}/${img}.png`} className="w-64"/>
         <h3 className="text-lg font-semibold mb-2">{title}</h3>
         <p>{content}</p>
--- app/skills/SkillsRow.tsx
+++ app/skills/SkillsRow.tsx
@@ -5,8 +5,8 @@ import { skillData } from "./skillData";
 export default function SkillsRow() {
   return (
     <Center flex>
-    {/* To-Do : replace grid to get contents centered. */}
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mx-auto">
+    {/* To-Do : uneven size with small screen */}
+      <div className="flex flex-wrap justify-center gap-16 max-w-128">
         {skillData &&
           skillData.map((skill, idx) => (
             <Skill
