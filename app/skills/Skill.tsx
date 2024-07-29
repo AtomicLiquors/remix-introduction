1135e17cac9a49f804163b5b9ed6d7c5d3157a42
chb <gyqls234@gmail.com>
2024-07-29 22:00:27 +0900

    :bento: skill image assets added

--- app/skills/Skill.tsx
+++ app/skills/Skill.tsx
@@ -5,6 +5,7 @@ export default function Skill({ title, content }: SkillProps) {
   return (
     <>
       <Center className="bg-white p-4 shadow-md rounded-lg">
+        <img src="/skills/java.png"/>
         <h3 className="text-lg font-semibold mb-2">{title}</h3>
         <p>{content}</p>
       </Center>
