037db5dac48ac492bdb3353a00d76190648e5842
chb <gyqls234@gmail.com>
2024-07-30 20:59:03 +0900

    :package: refactor package struture

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,27 +1,11 @@
-import Center from "@/components/common/Center";
 import Container from "@/components/common/Container";
-import RoundImage from "@/components/common/RoundImage";
+import IndexProfile from "@/pages/index/IndexProfile";
 import SkillsRow from "@/skills/SkillsRow";
-import { themeClasses } from "@/theme/theme";
 
 export default function Index() {
   return (
     <Container>
-      <Center flex>
-        <RoundImage src="/img/photos/profile.png" className="w-64"/>
-        <p
-          id="index-page"
-          className={`${themeClasses.text.primary} text-3xl font-bold underline`}
-        >
-          최 효 빈
-        </p>
-        <div className={themeClasses.text.secondary}>
-          <p>Java & JavaScript 개발자</p>
-          <p>Powered By Remix</p>
-        </div>
-        <div className={themeClasses.text.secondary}>| 010-2527-9030 · gyqls234@naver.com |</div>
-        <div>github.com/AtomicLiquors</div>
-      </Center>
+      <IndexProfile/>
       <SkillsRow />
     </Container>
   );
