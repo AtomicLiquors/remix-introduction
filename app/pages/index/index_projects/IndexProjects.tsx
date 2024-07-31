5b9fd5cc0f39dc82d3c90099599b449969a2f55f
chb <gyqls234@gmail.com>
2024-07-31 20:08:15 +0900

    :bug: added unique key to map items

--- app/pages/index/index_projects/IndexProjects.tsx
+++ app/pages/index/index_projects/IndexProjects.tsx
@@ -5,7 +5,7 @@ export default function IndexProjects() {
   return (
     <>
       {projectData &&
-        projectData.map((proj) => <Project {...proj}/>)}
+        projectData.map((proj, idx) => <Project key={idx} {...proj}/>)}
     </>
   );
 }
