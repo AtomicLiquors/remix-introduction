c36dd44e6cd3bdc1aa52ae0c9630c07445e6a724
chb <gyqls234@gmail.com>
2024-08-02 04:11:55 +0900

    :lipstick: education tab desing improved

--- app/pages/index/index_eductaions/IndexEducations.tsx
+++ app/pages/index/index_eductaions/IndexEducations.tsx
@@ -3,9 +3,9 @@ import { educationData } from "./educationData";
 
 export default function IndexEducations() {
   return (
-    <>
+    <ol className="relative border-s border-gray-200 dark:border-gray-700">   
       {educationData &&
         educationData.map((edu, idx) => <Education key={idx} {...edu} />)}
-    </>
+    </ol>
   );
 }
