5f28875f842b87f443634c8fb67c92d97174c4c1
chb <gyqls234@gmail.com>
2024-08-02 04:30:18 +0900

    :bug: fixed RowCard properly and applied to Project

--- app/pages/index/index_eductaions/IndexEducations.tsx
+++ app/pages/index/index_eductaions/IndexEducations.tsx
@@ -1,11 +1,14 @@
+import RowCard from "@/components/common/card/RowCard";
 import Education from "./Education";
 import { educationData } from "./educationData";
 
 export default function IndexEducations() {
   return (
-    <ol className="relative border-s border-gray-200 dark:border-gray-700">   
-      {educationData &&
-        educationData.map((edu, idx) => <Education key={idx} {...edu} />)}
-    </ol>
+    <RowCard>
+      <ol className="relative border-s border-gray-200 dark:border-gray-700">
+        {educationData &&
+          educationData.map((edu, idx) => <Education key={idx} {...edu} />)}
+      </ol>
+    </RowCard>
   );
 }
--- app/pages/index/index_projects/Project.tsx
+++ app/pages/index/index_projects/Project.tsx
@@ -1,59 +1,49 @@
 import { themeClasses } from "@/theme/theme";
 import { ProjectProps } from "./projectType";
+import RowCard from "@/components/common/card/RowCard";
 
 const Project: React.FC<ProjectProps> = (proj) => {
   return (
-    <div className="p-4 flex justify-center">
-      <div className="bg-white w-full lg:w-5/6 shadow-md border-r border-b border-l border-gray-200 p-4 flex sm:flex-col md:flex-col lg:flex-row-reverse justify-between leading-normal">
-        <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover bg-black rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden text-white">
-          image
+    <RowCard imgSrc="image">
+      <div>
+        <div className={`${themeClasses.text.primary} font-bold text-xl mb-2`}>
+          <span className="text-sm text-gray-600">{proj.emoji}</span>
+          &nbsp;
+          {proj.title}
         </div>
-        <div className="mb-8">
-          <div>
-            <div
-              className={`${themeClasses.text.primary} font-bold text-xl mb-2`}
-            >
-              <span className="text-sm text-gray-600">{proj.emoji}</span>
-              &nbsp;
-              {proj.title}
-            </div>
-            <div className="flex gap-4">
-              {/*
+        <div className="flex gap-4">
+          {/*
             {proj.stacks.map((stack, idx) => (
               <span key={idx} className="text-gray-700 text-base">{stack}</span>
             ))}
               */}
-            </div>
-            <div className={themeClasses.text.secondary}>
-              {proj.description}
-            </div>
-            <br></br>
-            <div className="font-bold">성과 및 리뷰</div>
-            {/*
+        </div>
+        <div className={themeClasses.text.secondary}>{proj.description}</div>
+        <br></br>
+        <div className="font-bold">성과 및 리뷰</div>
+        {/*
           <div>
             {proj.achievements.map((achievement, idx) => (
               <p key={idx} className="text-gray-700 text-base">- {achievement}</p>
             ))}
           </div>
            */}
-          </div>
-
-          {proj.links.code && (
-            <div className="flex items-center">
-              <img
-                className="w-10 h-10 rounded-full mr-4"
-                src="/img/github.png"
-                alt="Github"
-              />
-              <div className="text-sm">
-                <p className="text-gray-900 leading-none">| 소스 코드 |</p>
-                <p className="text-gray-600">{proj.links.code}</p>
-              </div>
-            </div>
-          )}
-        </div>
       </div>
-    </div>
+
+      {proj.links.code && (
+        <div className="flex items-center">
+          <img
+            className="w-10 h-10 rounded-full mr-4"
+            src="/img/github.png"
+            alt="Github"
+          />
+          <div className="text-sm">
+            <p className="text-gray-900 leading-none">| 소스 코드 |</p>
+            <p className="text-gray-600">{proj.links.code}</p>
+          </div>
+        </div>
+      )}
+    </RowCard>
   );
 };
 
