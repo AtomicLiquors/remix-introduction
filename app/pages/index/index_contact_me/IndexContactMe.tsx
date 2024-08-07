3508e52a00cb3af8811c162cccc31eeefd4c9999
chb <gyqls234@gmail.com>
2024-08-08 00:05:40 +0900

    :lipstick: Conatct Me Spacing

--- app/pages/index/index_contact_me/IndexContactMe.tsx
+++ app/pages/index/index_contact_me/IndexContactMe.tsx
@@ -1,14 +1,15 @@
 import Center from "@/components/common/Center";
+import { themeClasses } from "@/theme/theme";
 
 // To-Do : Zustand 설치와 화면 비활성화, 하단 파업 구현
 const handleMailButtonclick = () => {
-    alert('2kooong2');
+    alert('구현 준비 중입니다.');
 }
 
 export default function IndexContactMe() {
   return (
-    <Center>
-        <div className="text-h2">Contact Me!</div>
+    <Center flex className="h-64">
+        <div className={`${themeClasses.text.primary} font-bold text-h1`}>Contact Me!</div>
         <button onClick={handleMailButtonclick}>O</button>
     </Center>
   );
