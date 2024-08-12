adf034c2a9b7509527892c11c559242cb8af2dde
chb <gyqls234@gmail.com>
2024-08-12 12:58:28 +0900

    :art: contents of page directory has moved to routes dir or removed, application functional

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,8 +1,27 @@
-import Index from "@/pages/index/Index";
+
+import Container from "@/common/components/atoms/Container";
+import IndexCertificates from "@/routes/_index/index_certificates/IndexCertificates";
+import IndexContactMe from "@/routes/_index/index_contact_me/IndexContactMe";
+import IndexEducations from "@/routes/_index/index_eductaions/IndexEducations";
+import IndexProfile from "@/routes/_index/index_profile/IndexProfile";
+import IndexProjects from "@/routes/_index/index_projects/IndexProjects";
+import IndexSkills from "@/routes/_index/index_skills/IndexSkills";
+import IndexAwards from "@/routes/_index/index_awards/IndexAwards";
+
 
 export default function IndexRoute() {
 
   return (
-    <Index/>
+    <>
+      <IndexProfile />
+      <Container>
+        <IndexSkills />
+        <IndexProjects />
+        <IndexEducations />
+        <IndexAwards/>
+        <IndexCertificates/>
+        <IndexContactMe />
+      </Container>
+    </>
   );
 }
