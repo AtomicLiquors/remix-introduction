19006dc88a5b4d2c4d5ba8bc9bf5cbd533beb77d
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2024-08-15 20:10:41 +0900

    Revert ":heavy_plus_sign: added uiw markdown editor dependency"
    This reverts commit 79a8cc3eebbe239278d0f2a191b037da59d0adaa.

--- app/common/markdown/MarkdownRenderer.tsx
+++ app/common/markdown/MarkdownRenderer.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react';
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm';
 
 export default function MarkdownRenderer() {
 
-    const [markdown, setMarkDown] = useState('# type markdown here');
+    const markdown = '**한 보 도레미** *world*! \n  # Heading'
     return (
-        <>
-            <input className="border" type="textarea" onChange={(e) => setMarkDown(e.target.value)}/>
-            <ReactMarkdown className="prose" children ={markdown} remarkPlugins={[remarkGfm]}/>
-        </>
+        <ReactMarkdown className="prose" children ={markdown} remarkPlugins={[remarkGfm]}/>
     );
-}
+  }
+  
