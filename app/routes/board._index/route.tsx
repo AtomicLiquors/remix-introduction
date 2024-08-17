bd63b8641130ca4f4d5b6e1777135b062a6b84dd
chb <gyqls234@gmail.com>
2024-08-17 16:26:01 +0900

    :construct: click outside function on construction

--- app/routes/board._index/route.tsx
+++ app/routes/board._index/route.tsx
@@ -4,6 +4,7 @@ import { ActionFunction, ActionFunctionArgs, json } from "@remix-run/node";
 import MarkdownEditor from "@/common/markdown/MarkdownEditor";
 import BoardItem, { BoardItemProps } from "./components/BoardItem";
 import NewBoard from "./components/NewBoard";
+import { useState } from "react";
 
 export const loader = async () => {
   return await getBoards();
@@ -13,19 +14,23 @@ export default function BoardRoute() {
   const fetcher = useFetcher();
   const loading = fetcher.state !== "idle";
 
+  
+  const [isNewBoardFormVisibe, setIsNewBoardFormVisible] = useState(false);
   const result = useLoaderData<typeof loader>();
 
   //To-Do: Loading시 기존 화면 뿌옇게 표시.
 
   return (
     <>
+    
+      { isNewBoardFormVisibe ? <NewBoard /> : <button onClick={() => setIsNewBoardFormVisible(true)}>Click to show Form</button>}
       {result?.data!.map((board, idx) => (
         <BoardItem key={idx} {...(board as BoardItemProps)}></BoardItem>
       ))}
       {/*
       <MarkdownRenderer/>
        */}
-      <NewBoard />
+
       <MarkdownEditor />
     </>
   );
