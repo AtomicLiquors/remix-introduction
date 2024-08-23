0188780da1837db338989489b3a71b88433de38a
chb <gyqls234@gmail.com>
2024-08-23 23:06:01 +0900

    :bento: avatar asset renewal

--- app/common/avatar/avatarLinks.ts
+++ app/common/avatar/avatarLinks.ts
@@ -2,10 +2,12 @@ export const avatarSourcePrefix = "/img/avatars/"
 
 export const avatarSources = [
    "blank.png",
-    "avatar(1).jfif",
-    "avatar(2).jfif",
-    "avatar(3).jfif",
-    "avatar(4).jfif",
-    "avatar(5).jfif",
-    "avatar(6).jfif"
+    "avatar(1).jpg",
+    "avatar(2).jpg",
+    "avatar(3).jpg",
+    "avatar(4).jpg",
+    "avatar(5).jpg",
+    "avatar(6).jpg",
+    "avatar(7).jpg",
+    "avatar(8).jpg"
 ];
