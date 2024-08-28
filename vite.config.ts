36d26aa335d531d99fcd28fac939b96f4ff485fa
chb <gyqls234@gmail.com>
2024-08-28 13:15:28 +0900

    :bulb: comment on vite.config.ts regarding non-functional code

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,13 @@ import * as path from "path";
 
 export default defineConfig(() => {
   return {
+
+    /* Vercel 환경 변수 세팅 : 작동하지 않음. */
+    /* https://vercel.com/docs/frameworks/vite#environment-variables */
     define: {
       __VITE_CONTACT__: JSON.stringify(process.env.VITE_CONTACT),
     },
+
     plugins: [
       remix({
         ignoredRouteFiles: ["**/*.css"],
