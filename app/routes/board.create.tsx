3a5c44d18c73452dacaadeb24d67e294e50bb832
chb <gyqls234@gmail.com>
2024-08-28 15:59:18 +0900

    :safety_vest: action function validation

--- app/routes/board.create.tsx
+++ app/routes/board.create.tsx
@@ -1,5 +1,29 @@
 import { createBoard, BoardCreateRequestDTO } from "@/model/board.server";
 import { ActionFunction, ActionFunctionArgs, json } from "@remix-run/node";
+import { isValidAuthor, isValidContent, isValidPassword, isValidTitle } from "./board._index/util/verify";
+import { number } from "zod";
+
+const isValidCreateInput = (title: string, content: string, author: string, password: string): [result: boolean, msg: string] => {
+
+  let result: boolean = true;
+  let msg: string = "";
+
+  if (!isValidTitle(title)){
+    result = false;
+    msg = "제목";
+  }else if(!isValidContent(content)){
+    result = false;
+    msg = "내용";
+  }else if (!isValidAuthor(author)){
+    result = false;
+    msg = "작성자";
+  }else if(!isValidPassword(password)){
+    result = false;
+    msg = "비밀번호";
+  }
+
+  return [true, ""];
+}
 
 export const action: ActionFunction = async ({ request }: ActionFunctionArgs) => {
     const formData = await request.formData();
@@ -8,10 +32,16 @@ export const action: ActionFunction = async ({ request }: ActionFunctionArgs) =>
     const content = (formData.get('content') as string).trim();
     const author = (formData.get('author') as string).trim();
     const password = (formData.get('password') as string).trim();
-    const is_private = formData.get("is_private") as string;
+    const is_private = formData.get('is_private') as string;
   
     if (!title || !content || !author || !password) {
-      return json({ success: false, error: 'missing field' }, { status: 422 });
+      return json({ success: false, error: '비어 있는 입력값' }, { status: 422 });
+    }
+    
+    const [isValid, msg] = isValidCreateInput(title, content, author, password);
+
+    if (!isValid){
+      return json({ success: false, error: `유효하지 않은 ${msg}` }, { status: 422 });
     }
 
     const ip = request.headers.get('x-forwarded-for') || 'Unknown IP';
