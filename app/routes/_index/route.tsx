12d2e797ca0b2b413a40b733308d91fbc144cbbb
chb <gyqls234@gmail.com>
2024-08-29 20:20:22 +0900

    :label: created IndexIntroduction.tsx

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -8,23 +8,13 @@ import IndexAwards from "@/routes/_index/components/awards/IndexAwards";
 import { themeClasses } from "@/theme/theme";
 import Center from "@/common/components/atoms/Center";
 import IndexItemTitles from "./components/IndexItemTitles";
+import IndexIntroduction from "./components/introduction/IndexIntroduction";
 
 export default function IndexRoute() {
   return (
     <>
       <Container className="flex flex-col mt-20 gap-20">
-        <div>
-          <IndexItemTitles title="Introduction" />
-          <Center className={`mt-5 ${themeClasses.text.secondary}`}>
-            <p>안녕하세요! 프론트엔드 웹 개발자 최효빈입니다.</p>
-            <p>프론트엔드 분야의 다양한 기술과 빠른 피드백을 즐깁니다.</p>
-            <p>
-              사소한 아이디어도 진지하게 경청하고, 안 되는 이유보다 되는 이유를
-              찾으며
-            </p>
-            <p>색다른 소프트웨어를 만들어내는 팀원입니다.</p>
-          </Center>
-        </div>
+        <IndexIntroduction/>
         <IndexSkills />
         <IndexProjects />
         <IndexEducations />
