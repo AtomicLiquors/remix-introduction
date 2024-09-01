fd078c7bb86efa58eccb14f1be181a61ef0e7102
chb <gyqls234@gmail.com>
2024-09-02 02:04:02 +0900

    :label: function name correction on GridWithFarKeftItem

--- app/common/components/atoms/GridWithFarLeftItem.tsx
+++ app/common/components/atoms/GridWithFarLeftItem.tsx
@@ -7,7 +7,7 @@ interface GridWithFarLeftItemProps {
     down: ReactNode;
 }
 
-export default function IconGrid({className, left, up, down}: GridWithFarLeftItemProps) {
+export default function GridWithFarLeftItem({className, left, up, down}: GridWithFarLeftItemProps) {
   return (
     <div className={`grid grid-cols-[auto_1fr] grid-rows-2 ${className}`}>
       <div className="row-span-1 col-span-1 sm:row-span-2">
