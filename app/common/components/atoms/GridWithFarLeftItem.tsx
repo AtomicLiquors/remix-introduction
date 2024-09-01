7ac6b927a29e2c002ba056e0945f1c02af4033a3
chb <gyqls234@gmail.com>
2024-09-02 02:11:34 +0900

    :lipstick: applied grid to Skills

--- app/common/components/atoms/GridWithFarLeftItem.tsx
+++ app/common/components/atoms/GridWithFarLeftItem.tsx
@@ -9,7 +9,7 @@ interface GridWithFarLeftItemProps {
 
 export default function GridWithFarLeftItem({className, left, up, down}: GridWithFarLeftItemProps) {
   return (
-    <div className={`grid grid-cols-[auto_1fr] grid-rows-2 ${className}`}>
+    <div className={`grid grid-cols-[auto_1fr] grid-rows-[auto_1fr]  ${className}`}>
       <div className="row-span-1 col-span-1 sm:row-span-2">
         {left}
       </div>
