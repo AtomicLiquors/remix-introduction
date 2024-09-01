20dd5e0eb1d4787c1da8348dff4d752115390d86
chb <gyqls234@gmail.com>
2024-09-01 14:34:33 +0900

    :lipstick: LineBreak now put blank space at the end

--- app/routes/_index/components/eductaions/Education.tsx
+++ app/routes/_index/components/eductaions/Education.tsx
@@ -3,6 +3,7 @@ import TLItemTitle from "@/common/timeline/atoms/TLItemTitle";
 import TLItemCaption from "@/common/timeline/atoms/TLItemTime";
 import TLItemDescription from "@/common/timeline/atoms/TLItemContent";
 import { FONT_AWESOME_TYPES, TAILWIND_COLOR_TYPES } from "@/common/icon/FontAwesome";
+import LineBreak from "@/common/components/atoms/LineBreak";
 
 const Education: React.FC<EducationProps> = (edu) => {
   return (
@@ -10,8 +11,8 @@ const Education: React.FC<EducationProps> = (edu) => {
       <TLItemTitle title={edu.title} icon={FONT_AWESOME_TYPES.EDUCATION} color={TAILWIND_COLOR_TYPES.EDUCATION}/>
       <TLItemCaption>
         {edu.startDate.toLocaleDateString("ko-KR")} -{" "}
-        {edu.endDate.toLocaleDateString("ko-KR")} ({edu.time}시간) |{" "}
-        {edu.location}
+        {edu.endDate.toLocaleDateString("ko-KR")} ({edu.time}시간) <span className="hidden sm:inline">|</span>
+        <LineBreak>{edu.location}</LineBreak>
       </TLItemCaption>
       <TLItemDescription>
         {edu.content.map((line, idx) => (
--- app/routes/_index/components/introduction/IndexIntroduction.tsx
+++ app/routes/_index/components/introduction/IndexIntroduction.tsx
@@ -8,15 +8,22 @@ export default function IndexIntroduction() {
     <div>
       <IndexItemTitles title="Introduction" />
       <Center className={`mt-5 ${themeClasses.text.secondary}`}>
-        <p>안녕하세요!
-            <LineBreak>프론트엔드 웹 개발자 최효빈입니다.</LineBreak></p>
-        <p>프론트엔드 분야의 다양한 기술과
-            <LineBreak>빠른 피드백을 즐깁니다.</LineBreak></p>
         <p>
-          사소한 아이디어도 진지하게 경청하고,
+          <LineBreak>안녕하세요!</LineBreak>
+          <LineBreak>프론트엔드 웹 개발자 최효빈입니다.</LineBreak>
+        </p>
+        <p>
+          <LineBreak>프론트엔드 분야의 다양한 기술과</LineBreak>
+          <LineBreak>빠른 피드백을 즐깁니다.</LineBreak>
+        </p>
+        <p>
+          <LineBreak>사소한 아이디어도 진지하게 경청하고,</LineBreak>
           <LineBreak>안 되는 이유보다 되는 이유를 찾으며</LineBreak>
         </p>
-        <p>색다른 소프트웨어를<LineBreak>만들어내는 팀원입니다.</LineBreak></p>
+        <p>
+          <LineBreak>색다른 소프트웨어를</LineBreak>
+          <LineBreak>만들어내는 팀원입니다.</LineBreak>
+        </p>
       </Center>
     </div>
   );
