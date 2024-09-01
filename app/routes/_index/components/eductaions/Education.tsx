4d5801472c244c9e7d48008cf887b182c6fa01a9
chb <gyqls234@gmail.com>
2024-09-01 15:25:56 +0900

    :lipstick: education spacing

--- app/routes/_index/components/eductaions/Education.tsx
+++ app/routes/_index/components/eductaions/Education.tsx
@@ -2,21 +2,37 @@ import { EducationProps } from "./educationType";
 import TLItemTitle from "@/common/timeline/atoms/TLItemTitle";
 import TLItemCaption from "@/common/timeline/atoms/TLItemTime";
 import TLItemDescription from "@/common/timeline/atoms/TLItemContent";
-import { FONT_AWESOME_TYPES, TAILWIND_COLOR_TYPES } from "@/common/icon/FontAwesome";
+import {
+  FONT_AWESOME_TYPES,
+  TAILWIND_COLOR_TYPES,
+} from "@/common/icon/FontAwesome";
 import LineBreak, { breakpoint } from "@/common/components/atoms/LineBreak";
 
 const Education: React.FC<EducationProps> = (edu) => {
   return (
     <>
-      <TLItemTitle title={edu.title} icon={FONT_AWESOME_TYPES.EDUCATION} color={TAILWIND_COLOR_TYPES.EDUCATION}/>
+      <TLItemTitle
+        title={edu.title}
+        icon={FONT_AWESOME_TYPES.EDUCATION}
+        color={TAILWIND_COLOR_TYPES.EDUCATION}
+      />
       <TLItemCaption>
-        {edu.startDate.toLocaleDateString("ko-KR")} -{" "}
-        {edu.endDate.toLocaleDateString("ko-KR")} ({edu.time}시간) <span className="hidden sm:inline">|{" "}</span>
-        <LineBreak>{edu.location}</LineBreak>
+        <div className="block sm:flex">
+          <div className="mb-1 sm:mb-0">
+            {edu.startDate.toLocaleDateString("ko-KR")} -{" "}
+            {edu.endDate.toLocaleDateString("ko-KR")} ({edu.time}시간){" "}
+            <span className="hidden sm:inline">|&nbsp;</span>
+          </div>
+          <div>{edu.location}</div>
+        </div>
       </TLItemCaption>
       <TLItemDescription>
         {edu.content.map((line, idx) => (
-          <p className="text-sm" key={idx}>{line.split(breakpoint).map((e) => <LineBreak>{e}</LineBreak> )}</p>
+          <p className="text-sm" key={idx}>
+            {line.split(breakpoint).map((e) => (
+              <LineBreak>{e}</LineBreak>
+            ))}
+          </p>
         ))}
       </TLItemDescription>
     </>
