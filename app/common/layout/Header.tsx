45594437a92d86f1ee93816b5b44b5fa83f291c2
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2024-12-24 17:59:56 +0900

    :lipstick::package: created POSTS route & created Navbar.tsx and merged with Profile.tsx into Header.tsx

---
+++ app/common/layout/Header.tsx
@@ -0,0 +1,11 @@
+import Profile from "@/common/layout/header/Profile";
+import NavBar from "@/common/layout/header/Navbar";
+
+export default function Header() {
+    return (
+        <>
+            <Profile />
+            <NavBar />
+        </>
+    );
+}
