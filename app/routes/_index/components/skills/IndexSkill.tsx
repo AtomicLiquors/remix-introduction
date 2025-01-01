7219f1b5b78a80e6f8fe24ced03ac33251644e37
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-01-01 16:39:49 +0900

    :fire: removed comments from IndexSkill

--- app/routes/_index/components/skills/IndexSkill.tsx
+++ app/routes/_index/components/skills/IndexSkill.tsx
@@ -12,29 +12,6 @@ const IndexSkill: React.FC<SkillProps> = ({ title, content, img }) => {
     <div
       className={`${themeClasses.bg.card} flex gap-5 w-5/6 p-4 shadow-md rounded-lg text-left`}
     >
-      {/*
-      <img src={`${path}/${img}.png`} className="w-16 h-16" />
-      <div>
-        <h3
-          className={`${themeClasses.text.primary} text-lg text-primary font-semibold mb-2`}
-        >
-          {title}
-        </h3>
-
-        <BulletListContainer className={themeClasses.text.secondary}>
-          {content.map((line, lineIdx) => (
-            <BulletListItem
-              key={lineIdx}
-              spacing={1}
-              bullet={<>•</>}
-              item={line.split(breakpoint).map((token, tokenIdx) => (
-                <LineBreak key={tokenIdx}>{token}</LineBreak>
-              ))}
-            />
-          ))}
-        </BulletListContainer>
-      </div>
-      */}
       <GridWithFarLeftItem
         left={<img src={`${path}/${img}.png`} className="w-8 h-8 sm:w-16 sm:h-16" />}
         up={
