b94e9e740f2c0d4e569c8a0cc2542044e063892a
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-01-01 17:03:00 +0900

    :lipstick: useScrollReachedToImage applied to IndexSkill

--- app/routes/_index/components/skills/IndexSkill.tsx
+++ app/routes/_index/components/skills/IndexSkill.tsx
@@ -4,16 +4,22 @@ import { themeClasses } from "@/theme/theme";
 import BulletListContainer from "@/common/components/molecules/BulletListContainer";
 import BulletListItem from "@/common/components/atoms/BulletListItem";
 import GridWithFarLeftItem from "@/common/components/atoms/GridWithFarLeftItem";
+import { useScrollReachedToImage } from "@/hooks/useScrollReachedToImage";
+import { useEffect, useRef } from "react";
 
 const path = "/img/skills";
 
 const IndexSkill: React.FC<SkillProps> = ({ title, content, img }) => {
+
+  const imageRef = useRef<HTMLImageElement>(null);
+  const isImageVisible = useScrollReachedToImage(imageRef);
+
   return (
     <div
       className={`${themeClasses.bg.card} flex gap-5 w-5/6 p-4 shadow-md rounded-lg text-left`}
     >
       <GridWithFarLeftItem
-        left={<img src={`${path}/${img}.png`} className="w-8 h-8 sm:w-16 sm:h-16" />}
+        left={<img src={`${path}/${img}.png`} alt={`${title}Img`} ref={imageRef} className={`${isImageVisible ? 'opacity-100' : 'opcatiy-0'} w-8 h-8 sm:w-16 sm:h-16`}/>}
         up={
           <h3
             className={`${themeClasses.text.primary} ml-2 text-lg text-primary font-semibold mb-2`}
