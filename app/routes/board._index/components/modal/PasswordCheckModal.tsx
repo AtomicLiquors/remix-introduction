370e2e43b308bddab28deb1f86cee484734dda78
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-02-24 16:37:00 +0900

    :lipstick: preview boardItem on pwcheck modal

--- app/routes/board._index/components/modal/PasswordCheckModal.tsx
+++ app/routes/board._index/components/modal/PasswordCheckModal.tsx
@@ -3,11 +3,20 @@ import { Modal } from "@/common/modal/Modal";
 import { ModalSize } from "@/common/modal/type/ModalSizeType";
 import PasswordChecker from "../PasswordChecker";
 import { BoardItemType } from "../../types/BoardItemType";
-import { PWCheckOptionType } from "../../types/PasswordCheckOptionType";
+import { PWCheckOption, PWCheckOptionType } from "../../types/PasswordCheckOptionType";
 import { getCaptionByPWCheckOption } from "./util/getCaptionByPWCheckOption";
+import BoardItemBlockWrapper from "../boardItem/layout/BlockWrapper";
+import BoardItemFirstBlock from "../boardItem/layout/FirstBlock";
+import Avatar from "@/common/avatar/Avatar";
+import { dateToString } from "@/utils/date";
+import { title } from "process";
+import BoardItemTitles from "../boardItem/content/Titles";
+import BoardItemMiddleBlock from "../boardItem/layout/MiddleBlock";
 
-{/* To-Do: Desktop 뷰에서는 클릭 위치 근처에 보이게 가능할까? */}
-export const PasswordCheckModal = ({
+{
+  /* To-Do: Desktop 뷰에서는 클릭 위치 근처에 보이게 가능할까? */
+}
+export function PasswordCheckModal({
   isOpen,
   closeModal,
   boardItem,
@@ -17,30 +26,47 @@ export const PasswordCheckModal = ({
   closeModal: () => void;
   boardItem: BoardItemType | null;
   pwCheckOption: PWCheckOptionType;
-}) => (
-  <>
-    <Modal
-      isModalOpen={isOpen}
-      closeModal={closeModal}
-      modalSize={ModalSize.SMALL}
-      closeBtn
-    >
-      {boardItem ? (
-        <>
-          <Center>
-            <div className="text-gray-500">{getCaptionByPWCheckOption(pwCheckOption)}</div>
-            <div>{boardItem.post_id}번 게시글</div>
-            <PasswordChecker
-              post_id={boardItem.post_id}
-              onPwCheckPassed={function (): void {
-                throw new Error("Function not implemented.");
-              }}
-            />
-          </Center>
-        </>
-      ) : (
-        <div>선택된 게시글이 없습니다.</div>
-      )}
-    </Modal>
-  </>
-);
+}) {
+  return (
+    <>
+      <Modal
+        isModalOpen={isOpen}
+        closeModal={closeModal}
+        modalSize={ModalSize.SMALL}
+        closeBtn
+      >
+        {boardItem && (pwCheckOption != PWCheckOption.ViewDetail) && (pwCheckOption != PWCheckOption.None) ? (
+          <>
+            <Center>
+              <div className="text-gray-500">
+                {getCaptionByPWCheckOption(pwCheckOption)}
+              </div> 
+
+              <BoardItemBlockWrapper>
+                <BoardItemFirstBlock>
+                  <Avatar avatarId={boardItem.avatar_id} />
+                  <div className="text-sm">{boardItem.author}</div>
+                </BoardItemFirstBlock>
+                <BoardItemMiddleBlock>
+                  <BoardItemTitles title={boardItem.title} subtitle={boardItem.content} />
+                  <div className="inline-flex items-center text-xs font-normal text-gray-500 dark:text-gray-400">
+                    {dateToString(boardItem.created_at)}
+                  </div>
+                </BoardItemMiddleBlock>
+              </BoardItemBlockWrapper>
+
+              <PasswordChecker
+                post_id={boardItem.post_id}
+                onPwCheckPassed={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+              />
+            </Center>
+          </>
+        ) : (
+          <div>선택된 게시글이 없습니다.</div>
+        )}
+      </Modal>
+    </>
+  );
+}
